d9943ca1bc9d6a3cfeea90111d7bbd58edf8f439
ferReply <113031275+ferReply@users.noreply.github.com>
2022-09-17 15:49:03 +0200

    Update executeUrl.sh

--- .github/workflows/executeUrl.sh
+++ .github/workflows/executeUrl.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-echo "::set-output name=RESPONSE_GET::$(curl -s -o /dev/null -w %{http_code} $URL_REQ )";
+echo "::set-output name=RESPONSE_GET::$(curl -s -o /dev/null -w %{http_code} $URL_REQ )"
 if [ $RESPONSE_GET == 200 ]
 then
   echo "va bene socio"
