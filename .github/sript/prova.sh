d1a76142ca4e35c9fdf40dbf25b2d08d3b5362c4
ferReply <113031275+ferReply@users.noreply.github.com>
2022-09-17 15:49:40 +0200

    Create prova.sh

---
+++ .github/sript/prova.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo "::set-output name=RESPONSE_GET::$(curl -s -o /dev/null -w %{http_code} $URL_REQ )"
+if [ $RESPONSE_GET == 200 ]
+then
+  echo "va bene socio"
+else
+  echo "non va bene"
+fi
