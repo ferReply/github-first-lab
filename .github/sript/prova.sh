d6a4e81e575f8f17dc61179d2f38cf2b598a0222
ferReply <113031275+ferReply@users.noreply.github.com>
2022-09-18 13:18:50 +0200

    Update prova.sh

--- .github/sript/prova.sh
+++ .github/sript/prova.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 echo "::set-output name=RESPONSE_GET::$(curl -s -o /dev/null -w %{http_code} http.net/nexus/repository/IAP_Docker_Snapshot/v2/dsa-iam/jwks-auth-service/manifests/1.4.2-SNAPSHOT.eedfbbe )"
+echo "$RESPONSE_GET"
 if [ $RESPONSE_GET == 200 ]
 then
   echo "va bene socio"
