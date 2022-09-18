4c53aef2317075faafce9ddae93d08bdad103be6
ferReply <113031275+ferReply@users.noreply.github.com>
2022-09-18 14:08:30 +0200

    Update prova.sh

--- .github/sript/prova.sh
+++ .github/sript/prova.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-RESPONSE_GET=$(curl -s -o /dev/null -w %{http_code} https://iap.bmwgroup.net/nexus/repository/IAP_Docker_Snapshot/v2/dsa-iam/jwks-auth-service/manifests/1.4.2-SNAPSHOT.eedfbbe )"
+echo "::set-output name=RESPONSE_GET::$(curl -s -o /dev/null -w %{http_code} http.net/nexus/repository/IAP_Docker_Snapshot/v2/dsa-iam/jwks-auth-service/manifests/1.4.2-SNAPSHOT.eedfbbe )"
 echo $RESPONSE_GET
 
 case $RESPONSE_GET in
@@ -15,10 +15,3 @@ case $RESPONSE_GET in
     echo "ERRORE"
     ;;
 esac
-
-# if [ $RESPONSE_GET == 200 ]
-# then
-#   echo "va bene socio"
-# else
-#   echo "non va bene"
-# fi
