ede7b123cee080a69f93893c71c496f62288b750
ferReply <113031275+ferReply@users.noreply.github.com>
2022-09-18 13:26:27 +0200

    Update prova.sh

--- .github/sript/prova.sh
+++ .github/sript/prova.sh
@@ -2,9 +2,22 @@
 
 echo "::set-output name=RESPONSE_GET::$(curl -s -o /dev/null -w %{http_code} http.net/nexus/repository/IAP_Docker_Snapshot/v2/dsa-iam/jwks-auth-service/manifests/1.4.2-SNAPSHOT.eedfbbe )"
 echo "$RESPONSE_GET"
-if [ $RESPONSE_GET == 200 ]
-then
-  echo "va bene socio"
-else
-  echo "non va bene"
-fi
+
+case $RESPONSE_GET in
+  200)
+    echo "200"
+    ;;
+  404)
+    echo "404"
+    ;;
+   *)
+    exit 1
+    ;;
+esac
+
+# if [ $RESPONSE_GET == 200 ]
+# then
+#   echo "va bene socio"
+# else
+#   echo "non va bene"
+# fi
