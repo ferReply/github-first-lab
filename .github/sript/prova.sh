d21b069bc486a8f95006f265a32bb596995e07f4
ferReply <113031275+ferReply@users.noreply.github.com>
2022-09-18 14:21:36 +0200

    Update prova.sh

--- .github/sript/prova.sh
+++ .github/sript/prova.sh
@@ -1,17 +1,14 @@
 #! /bin/bash
 
-echo "::set-output name=RESPONSE_GET::$(curl -s -o /dev/null -w %{http_code} http.net/nexus/repository/IAP_Docker_Snapshot/v2/dsa-iam/jwks-auth-service/manifests/1.4.2-SNAPSHOT.eedfbbe )"
-echo $RESPONSE_GET
-
-case $RESPONSE_GET in
+case $1 in
   200)
-    echo "response: $RESPONSE_GET"
-    echo "description: The image "
+    echo "description: The image exist"
     ;;
   404)
     echo "404"
     ;;
    *)
     echo "ERRORE"
+    curl $2
     ;;
 esac
