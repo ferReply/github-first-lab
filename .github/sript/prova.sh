42c0f7cb0412683a25e26c234ec2de06d5caf367
ferReply <113031275+ferReply@users.noreply.github.com>
2022-09-18 13:47:27 +0200

    Update prova.sh

--- .github/sript/prova.sh
+++ .github/sript/prova.sh
@@ -2,7 +2,6 @@
 
 RESPONSE_GET=$(curl -s -o /dev/null -w %{http_code} https://iap.bmwgroup.net/nexus/repository/IAP_Docker_Snapshot/v2/dsa-iam/jwks-auth-service/manifests/1.4.2-SNAPSHOT.eedfbbe )
 echo "::set-output name=RESULT::$RESPONSE_GET"
-if [$RESPONSE_GET == 200]
 
 case $RESPONSE_GET in
   200)
