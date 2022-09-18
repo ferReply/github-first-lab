1950480124bfff55c43bf624af4365eaf8f8ef65
ferReply <113031275+ferReply@users.noreply.github.com>
2022-09-18 13:52:07 +0200

    Update prova.sh

--- .github/sript/prova.sh
+++ .github/sript/prova.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-RESPONSE_GET=$(curl -s -o /dev/null -w %{http_code}  https://iap.bmwgroup.net/nexus/repository/IAP_Docker_Snapshot/v2/dsa-iam/jwks-auth-service/manifests/1.4.2-SNAPSHOT.eedfbbe)
+echo "::set-output name=RESPONSE_GET::$(curl -s -o /dev/null -w %{http_code}  https://iap.bmwgroup.net/nexus/repository/IAP_Docker_Snapshot/v2/dsa-iam/jwks-auth-service/manifests/1.4.2-SNAPSHOT.eedfbbe)"
 echo "::set-output name=RESULT::$RESPONSE_GET"
 
 case $RESPONSE_GET in
