2a775c7833828bc95247084724953a94f093816d
ferReply <113031275+ferReply@users.noreply.github.com>
2022-09-18 15:20:53 +0200

    Update prova.sh

--- .github/sript/prova.sh
+++ .github/sript/prova.sh
@@ -1,20 +1,20 @@
 #! /bin/bash
 
-if [ $1 == 200 ]
+if [ $0 == 200 ]
 then 
   echo "ok"
 fi
 
-case $1 in
+case $0 in
   200)
-    echo "description: The image $3 exist"
+    echo "description: The image $2 exist"
     ;;
   404)
     echo "404"
     ;;
    *)
-    echo "ERRORE_SOCIO - $1"
+    echo "ERRORE_SOCIO - $0"
     echo "fai cagare"
-    curl $2
+    curl $1
     ;;
 esac
